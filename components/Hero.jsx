8fde7b5559d9ba3cc0a5c46bc3269eb454550c52
0-martin <000macharia@gmail.com>
2022-12-20 13:13:20 +0300

    initial commit

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 
 const Hero = ({ heading, message, action, onClicked, title, text, logo }) => {
   return (
-    <div className='flex items-center justify-center h-screen bg-fixed bg-center bg-cover custom-img'>
+    <div className='flex items-center justify-center  bg-fixed bg-center bg-cover custom-img'>
       {/* Overlay */}
       <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/70 z-[2]' />
       <div className='p-4 text-white z-[2] mt-[4rem]'>
